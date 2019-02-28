f266db69d2fddbdd13b3c4fadb37587020e07222
git-user <git-email>
2019-02-28 17:49:17 +0000

    trying to get name right

---
+++ rsfbpic/rswake/lbn_wake.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+"""Calculations from 2017 PRAB article by Lebedev, Burov and Nagaitsev (LBN)
+
+:copyright: Copyright (c) 2019 Radiasoft LLC. All Rights Reserved.
+:license: http://www.apache.org/licenses/LICENSE-2.0.html
+"""
+import math
+
+# tbd
