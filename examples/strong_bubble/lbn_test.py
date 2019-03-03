f56f7dfe863a318ba4ae57e5f2f44565ae6ffdd5
David Bruhwiler <bruhwiler@radiasoft.net>
2019-03-02 20:24:06 -0700

    removing .../python_scripts/pwfa/serial/ and image file

--- examples/strong_bubble/lbn_test.py
+++ examples/strong_bubble/lbn_test.py
@@ -1,6 +1,7 @@
 import math
 import numpy as np
 from rsfbpic import rswake
+from rsbeams.rsphysics import rsconst
 
 # Specify default values of some physical quantities...
 #   These are motivated by upcoming PWFA experiments at FACET II
@@ -12,7 +13,7 @@ n_pe     = n_pe_cgs * 1.e6   #  [m^-3]
 # electron plasma frequency, wavenumber, wavelength
 om_pe     = np.sqrt(n_pe*rsconst.e**2
                     / (rsconst.m_e*rsconst.epsilon_0) )  # [rad/s]
-k_pe      = om_pe/constants.c                    # [rad/m]
+k_pe      = om_pe/rsconst.c                      # [rad/m]
 lambda_pe = 2.*np.pi/k_pe                        # [m]
 lambda_pe_microns = lambda_pe*1.e6               # [microns]
 
@@ -29,11 +30,11 @@ beam_rms_r = 0.4 / k_pe  # [m]   satisfies resonance condition
 beam_rms_z = 1.2 / k_pe  # [m]   satisfies resonance condition
 
 # total charge in the electron drive beam
-# beam_tot_q = 1.e10*math.abs(rsconst.e)    # [C]   FACET II params...?
+# beam_tot_q = 1.e10*np.abs(rsconst.e)    # [C]   FACET II params...?
 beam_tot_q = 3.e-9                          # [C]   3 nC
 # peak number density of the drive beam
 beam_rms_v = math.pi*beam_rms_z*beam_rms_r**2
-beam_rms_n = beam_tot_q/math.abs(rsconst.e)/beam_rms_v
+beam_rms_n = beam_tot_q/np.abs(rsconst.e)/beam_rms_v
 
 # large density ratio is required for blowout regime
 dens_ratio = beam_rms_n/n_pe    # should be >> unity
@@ -49,3 +50,4 @@ wb_rms_z = 0.036 * lambda_pe  # [m] RMS length
 wb_tot_q = 0.100e-9           # [C] total charge of 100 pC
 wb_gamma = 100                # relativistic gamma factor
 wb_trail = 0.900 * lambda_pe  # [m] trailing distance behind center of drive beam
+
